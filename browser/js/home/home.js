19de0be644de3464e0e926575ce37d059b271708
DJ Nadgar <dj@exciteresearch.com>
2015-05-07 18:54:08 -0400

    Initial GUI with working iframe, nonworking editor, console and buttons.

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -5,3 +5,28 @@ app.config(function ($stateProvider) {
         templateUrl: 'js/home/home.html'
     });
 });
+
+app.controller('PlayCanvasCtrl',function($scope,$sce){
+	//playCanvas URL can be changed to anything including /pc/index.html or http://playcanv.as/p/aP0oxhUr
+	$scope.playCanvasURL = $sce.trustAsResourceUrl('/pc/index.html');
+});
+
+app.controller('CodeEditorCtrl',function($scope,$sce){
+	//Could also be a Panel of Tabs
+	
+});
+
+app.controller('CodeConsoleCtrl',function($scope,$sce){
+	//Code output, console logs, errors etc.
+	
+});
+
+app.controller('ButtonsCtrl',function($scope,$sce){
+	//Practice and/or Compete
+	
+});
+
+app.controller('SideMenuCtrl',function($scope,$sce){
+	//Chat, Repo, FAQ. etc
+	
+});
