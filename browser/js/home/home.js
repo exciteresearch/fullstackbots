40b9c3262e1ef4269cd1f1bf22ce1c6a2df406d7
DJ Nadgar <dj@exciteresearch.com>
2015-05-08 09:55:48 -0400

    successful  get osc.js

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,4 +1,5 @@
 'use strict';
+
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
@@ -6,45 +7,58 @@ app.config(function ($stateProvider) {
     });
 });
 
-app.controller('PlayCanvasCtrl',function($scope,$sce){
-	//playCanvas URL can be changed to anything including /pc/index.html or http://playcanv.as/p/aP0oxhUr
-	$scope.playCanvasURL = $sce.trustAsResourceUrl('http://playcanv.as/p/aP0oxhUr');
+//app.service('srcCode', function($q) {
+//	  var d = $q.defer();
+//	  var getData = $http({
+//		  url: '/pc/osc.js',
+//		  method: 'GET',
+//		  transformResponse: appendTransform($http.defaults.transformResponse, function(value) {
+//		    return doTransform(value);
+//		  })
+//		});
+//	  
+//	  this.botCode = function() {
+//	    fs.readFile('tags', 'utf8', function(err, data) {
+//	      if (err) throw err;
+//	      console.debug(data.split(','));
+//	      d.resolve(data.split(','));
+//	    });
+//	    return d.promise();
+//	  };    
+//	});
+
+
+app.factory('botCodeFactory', function ($http) {
+    return {
+        getBotCode: function () {
+            var queryParams = {};
+//            if (category) {
+//                queryParams.category = category;
+//            }
+            return $http.get('/pc/osc.js', {
+                params: queryParams
+            }).then(function (response) {
+                return response.data;
+            });
+        },
+
+        sendBotCode: function (data) {
+            return $http.post('/pc', data).then(function (response) {
+                return response.data;
+            });
+        }
+    };
 });
 
-app.controller('CodeEditorCtrl',function($scope){
+app.controller('PlayCanvasCtrl',function($scope,$sce){
+	//playCanvas URL can be changed to anything including /pc/index.html or http://playcanv.as/p/aP0oxhUr
+	$scope.playCanvasURL = $sce.trustAsResourceUrl('/pc/index.html');
+});
+
+app.controller('CodeEditorCtrl',function($scope, botCodeFactory){
 	//Could also be a Panel of Tabs
-	$scope.string = "pc.script.create('oscillator', function (app) {" + "\n" +
-	"\t" + "var Oscillator = function (entity) {" + "\n" +
-	"\t" + "\t" + "this.entity = entity;" + "\n" +
-	"\t" + "\t" + "this.paused = false; // paused state" + "\n" +
-	"\t" + "\t" + "this.amplitude = 10; // The amount to oscillate" + "\n" +
-	"\t" + "\t" + "this.time = 0; // The time value for the oscillation" + "\n" +
-	"\t" + "};" + "\n" +
-	"" + "\n" +
-	"\t" + "// define the update function" + "\n" +
-	"\t" + "Oscillator.prototype = {" + "\n" +
-	"\t" + "\t" + "update: function (dt) {" + "\n" +
-	"\t" + "\t" + "// Use the keyboard handler from the Application" + "\n" +
-	"\t" + "\t" + "// to pause/unpause" + "\n" +
-	"\t" + "\t" + "if (app.keyboard.wasPressed(pc.KEY_SPACE)) {" + "\n" +
-	"\t" + "\t" + "\t" + "\t" + "this.paused = !this.paused; // toggle paused state" + "\n" +
-	"\t" + "\t" + "}" + "\n" +
-	"" + "\n" +
-	"\t" + "\t" + "if (!this.paused) {" + "\n" +
-	"\t" + "\t" + "// increment the time value by the frametime" + "\n" +
-	"\t" + "\t" + 	"\t" + "\t" + "this.time += dt;" + "\n" +
-	"" + "\n" +
-	"\t" + "\t" + 	"\t" + "\t" + 	"// Calculate the new value" + "\n" +
-	"\t" + "\t" + 	"\t" + "\t" + "var x = this.amplitude * Math.sin(this.time*10);" + "\n" +
-	"" + "\n" +
-	"\t" + "\t" + 	"\t" + "\t" + 	"// Update the x position of the Entity" + "\n" +
-	"\t" + "\t" + 	"\t" + "\t" + 	"this.entity.setLocalPosition(x, 0, 0);" + "\n" +
-	"\t" + "\t" + 	"}" + "\n" +
-	"\t" + "}" + "\n" +
-	"};" + "\n" +
-	"// return the class definition" + "\n" +
-	"return Oscillator;" + "\n" +
-	"});";	
+	$scope.string = "";
+	botCodeFactory.getBotCode().then(function(data){ console.log(data); $scope.string = data;});
 });
 
 app.controller('CodeConsoleCtrl',function($scope,$sce){
